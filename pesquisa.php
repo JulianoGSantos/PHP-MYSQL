d1664684b9cb4ba2f08b4a6c1ae1feb51f871515
juliano <jot.asa@hotmail.com>
2024-02-01 18:37:52 -0300

    PDO

--- pesquisa.php
+++ pesquisa.php
@@ -6,29 +6,31 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <title>Pesquisa</title>
 </head>
-    <body>
-        <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand">Pesquisar</a>
-                <form class="d-flex" action="pesquisa.php" method="post" role="search">
-                <input class="form-control me-2" name="busca" type="search" placeholder="Nome" aria-label="busca">
-                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-                </form>
-            </div>
-        </nav>
-        <?php
-            include("database-connection.php");
-            $busca = $_POST['busca'];
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand">Pesquisar</a>
+            <form class="d-flex" action="pesquisa.php" method="post" role="search">
+            <input class="form-control me-2" name="busca" type="search" placeholder="Nome" aria-label="busca">
+            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+            </form>
+        </div>
+    </nav>
+    <?php
+        include("database-connection.php");
+        $busca = $_POST['busca'];
 
-            $sql = "SELECT * FROM pessoas WHERE nome LIKE :%busca%;";
-            $res = $pdo->prepare($sql);
-            $res->execute(['busca' => $busca]);
-            $members = $res->fetch();
-        ?>
-            
-            <?php foreach ($members as $member):?>
-            <tr>
-                <td><?=$member['nome'];?></td>
-            </tr>    
-    </body>
-</html>.
+        $sql = "SELECT nome, endereco, telefone, email, data_nascimento FROM pessoas cod_pessoas = :id;";
+        $statement = $pdo->prepare($sql);
+        $statement->execute(['cod_pessoa'=>$cod_pessoa]);
+        $pessoas = $statement->fetch();        
+        if(!$pessoas){
+            include 'page-not-found.php';
+        }
+
+        var_dump($pessoas);
+
+    ?>
+
+</body>
+</html>
