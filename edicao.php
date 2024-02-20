1da16bb7e2d7d47e994522063574b15cc616fbf4
juliano <jot.asa@hotmail.com>
2024-02-20 19:49:31 -0300

    submit_id_page_edit

--- edicao.php
+++ edicao.php
@@ -28,27 +28,12 @@
         <div class="row">
             <div class="col">
                 <h1>Edição</h1>
-                <form action="edicao_script.php?cod_pessoa<?= $id ?>" method="post">
+                <form action="edicao_script.php?id=<?= $id ?>" method="post">
                     <div class="form-group mt-2">
                         <label for="nome">Nome Completo</label>
                         <input type="text" name="nome" class="form-control" value="<?= htmlspecialchars($pessoas['nome'])?>">
                     </div>
-                    <div class="form-group mt-2">
-                    <label for="endereco">Endereço</label>
-                        <input type="text" name="endereco" class="form-control" value="<?= htmlspecialchars($pessoas['endereco'])?>">
-                    </div>
-                    <div class="form-group mt-2">
-                    <label for="telefone">Telefone</label>
-                        <input type="text" name="telefone" class="form-control" value="<?= htmlspecialchars($pessoas['telefone'])?>">
-                    </div>
-                    <div class="form-group mt-2">
-                    <label for="email">Email</label>
-                        <input type="email" name="email" class="form-control" value="<?= htmlspecialchars($pessoas['email'])?>">
-                    </div>
-                    <div class="form-group mt-2">
-                    <label for="data_nascimento">Data de Nascimento</label>
-                        <input type="date" name="data_nascimento" class="form-control" value="<?= htmlspecialchars($pessoas['data_nascimento'])?>">
-                    </div>
+                    
                     <div class="form-group mt-2">
                         <input type="submit" class="btn btn-primary" value="salvar alterações">
                     </div>
--- edicao_script.php
+++ edicao_script.php
@@ -13,32 +13,15 @@
                 <?php
                     include('database-connection.php');
                     
-                    $id = filter_input(INPUT_GET, 'cod_pessoa', FILTER_VALIDATE_INT);
-                    if(!$id){
-                        include 'page-not-found.php';
-                    }
-
+                    $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                    $nome = $_POST['nome'];
                     if($id){
-                        $sql = "UPDATE pessoas
-                                SET nome = :nome, endereco = :endereco, telefone = :telefone, email = :email, data_nascimento = :data_nascimento
-                                WHERE cod_pessoa = :cod_pessoa;";
-                        $res = $pdo->query($sql);
-
-                        if ($res == true) {
-                            echo "<script>alert('Edição de $nome salva com sucesso')</script>";
-                        } else {
-                            echo "<script>alert('Não foi possível salvar as alterações')</script>";
-                        }
+                        echo "deu certo</br>";
+                        var_dump($id);
+                    }else{
+                        echo "deu errado";
                     }
                 ?>
-                <div class="row g-4 justify-content-center mt-5">
-                    <div class="col-1 p-1">
-                        <a href="javascript:history.back(-1)" class="btn btn-primary">voltar</a>
-                    </div>
-                    <div class="col-3 p-1">
-                    <a href="index.php" class="btn btn-info">ir para o início</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
