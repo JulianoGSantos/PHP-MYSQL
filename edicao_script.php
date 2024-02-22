aa2ede926f1f31a4d5c07a5f8eeef3761c9d97f3
juliano <jot.asa@hotmail.com>
2024-02-22 18:28:17 -0300

    edit_continue2

--- edicao_script.php
+++ edicao_script.php
@@ -12,36 +12,22 @@
             <div class="col">
                 <?php
                     include('database-connection.php');
-                    
+
                     $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
                     $nome = $_POST['nome'];
                     $endereco = $_POST['endereco'];
                     $telefone = $_POST['telefone'];
                     $email = $_POST['email'];
                     $data_nascimento = $_POST['data_nascimento'];
-
-                    if($id && $nome && $endereco && $telefone && $email && $data_nascimento){
-                        try{
-                            $pdo->beginTransaction();
-                            $sql = $pdo->prepare("UPDATE pessoas
-                                    SET nome = :nome, endereco = :endereco, telefone = :telefone, email = :email, data_nascimento = :data_nascimento
-                                    WHERE id = :id;");
-
-                            $sql->bindValue(':nome',$nome);
-                            $sql->bindValue(':endereco',$endereco);
-                            $sql->bindValue(':telefone',$telefone);
-                            $sql->bindValue(':email',$email);
-                            $sql->bindValue(':data_nascimento',$data_nascimento);
-                            $sql->bindValue(':id',$id);
-
-                            $pdo->commit();
-
-                            header("Location: pesquisa.php");
-                            exit;
-                        }catch(PDOException $e){
-                            throw $e;
-                        }
+                    if($id){
+                        $sql =  "UPDATE pessoas
+                                SET nome = :nome, endereco = :endereco, telefone = :telefone, email = :email, data_nascimento = :data_nascimento
+                                WHERE id = :id:";
+                    }else{
+                        unset($pessoas['id']);
+                        $sql = "INSERT INTO pessoas (nome, endereco, telefone, email, data_nascimento) VALLUES(:nome, :endereco, :telefone, :email, :data_nascimento);";
                     }
+                    
                 ?>
             </div>
         </div>
