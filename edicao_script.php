6cfba0580519686d20efc72c06c8d88c71ffcd27
juliano <jot.asa@hotmail.com>
2024-02-23 20:12:27 -0300

    debugging

--- edicao_script.php
+++ edicao_script.php
@@ -1,7 +1,7 @@
 <?php
     include('database-connection.php');
-
-    $$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    include('functions.php');
+    $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
     $pessoas = [
         'id' => $id,
         'nome' => '',
@@ -11,16 +11,25 @@
         'data_nascimento' => '',
     ];
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $pessoas['nome'] = $_POST['nome'];
-        $pessoas['endereco'] = $_POST['endereco'];
-        $pessoas['telefone'] = $_POST['telefone'];
-        $pessoas['email'] = $_POST['email'];
-        $pessoas['data_nascimento'] = $_POST['data_nascimento'];
-    }else{
-        $arguments = $pessoas;
-        if($id){
-            $sql = "UPDATE ";
-        }
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $pessoas['nome'] = $_POST['nome'];
+    $pessoas['endereco'] = $_POST['endereco'];
+    $pessoas['telefone'] = $_POST['telefone'];
+    $pessoas['email'] = $_POST['email'];
+    $pessoas['data_nascimento'] = $_POST['data_nascimento'];
+
+    $arguments = $pessoas;
+    if($id){
+        $sql = "UPDATE pessoas
+                SET nome = :nome, endereco = :endereco, telefone = :telefone, email = :email, data_nascimento = :data_nascimento
+                WHERE id = :id;";
     }
+    try{
+        pdo($pdo, $sql);
+        echo "<script>alert('Alteração cadastrada com sucesso')</script>";
+        header("Location: pesquisa.php");
+    }catch(PDOException $e){
+        throw $e;
+    }
+}    
 ?>
